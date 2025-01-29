f9281698c96a95e4b6612466517d0bd8ba9c45b2
Haoxuan Huang <3025886H@student.gla.ac.uk>
2025-01-29 11:22:49 +0000

    Completed Chapter 5 - Models and Databases setup

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = "Categories"  # **注意这个拼写**
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)  # 添加 views 字段，默认值为0
+
+    def __str__(self):
+        return self.title
+
